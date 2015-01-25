3e4c844cc6c4b9fb60036d50e8b3e5d41ef8bbab
Piotr Zawiślak <piotr.za@webinterpret.com>
2015-01-26 00:56:48 +0100

    Exploratory Data Analysis, Project 02, plot1.R done

---
+++ plot1.R
@@ -0,0 +1,7 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+years <-tapply(NEI$Emissions, NEI$year, sum)
+
+png(file = "plot1.png", width = 480, height = 480)
+barplot(years,col=years)
+
